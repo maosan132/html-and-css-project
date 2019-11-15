5cefde7d36911eb1d124e67f537295173306202c
Ricardo Ribeiro <rcribeiro@me.com>
2019-11-15 13:03:40 -0300

    changing bash script

--- bash/script.sh
+++ bash/script.sh
@@ -3,7 +3,10 @@ first_greeting="Nice to meet you!"
 later_greeting="How are you?"
 greeting_occasion=0
 
-while [ $greeting_occasion -lt 3 ]
+echo "How many times should I greet?"
+read greeting_limit
+
+while [ $greeting_occasion -lt $greeting_limit ]
 do
   if [ $greeting_occasion -lt 1 ]
   then
